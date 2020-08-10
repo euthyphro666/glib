573d62a0bdfd41bd09a04c99d9acdc8747e89366
Josh <joshhess13@gmail.com>
2020-08-10 17:53:49 -0600

    Cmake setup, opengl vcpkg's added, main class with basic gl stuff.

---
+++ README.md
@@ -0,0 +1,9 @@
+# GLIB
+
+The OpenGL Intermediate Bullshit!
+
+Welcome to my playground of experimenting with OpenGl.
+I hope to learn more about modern OpenGl and better methods
+to render 2D graphics.
+
+Wish me luck!
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// Dimensions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main() {
+
+    // Init gl
+    if (!glfwInit())
+    {
+        printf("GLFW initialization failed!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup window props
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // Core profile means no backward compatibility. Only modern gl code.
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    auto *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Playground", NULL, NULL);
+    if (!mainWindow)
+    {
+        printf("GLFW window creation failed.");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Grab opengl window buffer
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+
+    // Set context for GLFW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extensions
+    glewExperimental = GL_TRUE;
+
+    if (!glewInit()) {
+        printf("GLEW initialization failed.");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup viewport
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // Handle glfw events
+        glfwPollEvents();
+
+        glClearColor(1.0, 0.8, 0.2, 1.0);
+    }
+
+
+
+    return 0;
+}
